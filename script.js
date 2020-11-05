3c2e6abfeabef7c99e8cb0e2b90aed3b58b28ba0
Anıl Özdemir <32442212+anilozdemir96@users.noreply.github.com>
2020-11-05 11:26:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,356 @@
+const data = {
+  "nodes": [{'type': 'Individual', 'value': 'Bakkal Bedri Efendi', 'code': 'Individual'},
+ {'type': 'Individual', 'value': 'Yüzbaşı Hüseyin Ağa', 'code': 'Individual'},
+ {'type': 'Individual', 'value': 'Hüseyin Hakkı Efendi', 'code': 'Individual'},
+ {'type': 'Individual', 'value': 'Esna Hanım', 'code': 3},
+ {'type': 'Individual', 'value': 'Alaybeyi müteveffa Hasan Bey', 'code': 4},
+ {'type': 'Individual', 'value': 'Osmanapola Aliye Hanım', 'code': 5},
+ {'type': 'Individual', 'value': 'Gemici Hamid Dıraro Arifaki', 'code': 6},
+ {'type': 'Individual', 'value': 'Nuriye binti İbrahim Lukaki', 'code': 7},
+ {'type': 'Individual', 'value': 'Salih Camadaki', 'code': 8},
+ {'type': 'Individual', 'value': 'Müteveffa Mehmed Reis', 'code': 9},
+ {'type': 'Individual', 'value': 'Saliha Çiçopola', 'code': 10},
+ {'type': 'Individual', 'value': 'Penbe Hanım', 'code': 11},
+ {'type': 'Individual', 'value': 'Zenci Bekir', 'code': 12},
+ {'type': 'Individual', 'value': 'Fuznaraki Hayati Efendi', 'code': 13},
+ {'type': 'Individual',
+  'value': "Yüzbaşı Hüseyin Ağa'nın eytamı Kazım",
+  'code': 14},
+ {'type': 'Individual',
+  'value': "Yüzbaşı Hüseyin Ağa'nın eytamı Fatma",
+  'code': 15},
+ {'type': 'Individual', 'value': 'Kazım Efendi', 'code': 16},
+ {'type': 'Individual', 'value': ' Gemici Hamid Dra Arifaki', 'code': 17},
+ {'type': 'Individual', 'value': 'Pirinaki Emin', 'code': 18},
+ {'type': 'Individual',
+  'value': "Saliha Çiçopola'nın biraderi Bekir",
+  'code': 19},
+ {'type': 'Individual',
+  'value': "Yüzbaşı Hüseyin Ağa'nın eytamı Şahsene",
+  'code': 20},
+ {'type': 'Individual',
+  'value': '\xa0bekraki Hüseyin Hakkı Efendi',
+  'code': 21},
+ {'type': 'Individual', 'value': ' Esna Hanım', 'code': 22},
+ {'type': 'Individual', 'value': 'Berber Hacı Ali', 'code': 23},
+ {'type': 'Individual', 'value': 'Mustafa Bey', 'code': 24},
+ {'type': 'Document',
+  'value': 'Kumkapı Mahallesi’nden Bakkal Bedri Efendi’nin eşi Saliha Çiçopola’nın biraderi Bekir’in yaptığı tazyik ve icbar üzerine evlerindeki eşyalarla firar ettiğine, bu kişilerin celbiyle şeri şerifin icrasını talep ettiğine dair dilekçesi. [18 Teşrin-i Evvel 1306]',
+  'code': 25},
+ {'type': 'Document',
+  'value': "Kumkapı Mahallesi sakinlerinden Bakkal Bedri Efendi kendisini boşamış ise de mühr-ü müeccel olan para asadan çekinmekte olduğu için celbiyle mühr-ü müeccelin tahsil ettirilmesi ve nafakasının belirlenmesine dair Penbe Hanım'ın dilekçesi. [4 Temmuz 1304]",
+  'code': 26},
+ {'type': 'Document',
+  'value': "Yüzbaşı Hüseyin Ağa eytamının Kastel Caddesi'ndeki mutasarrıf olduğu hane hissesinin tuvaletlerin temizlenmesi için 13 kuruş 12 paranın Hanya Eytam Sandığı'ndan alındığına dair Zenci Bekir imzalı pusula. [22 Şubat 1307]",
+  'code': 27},
+ {'type': 'Document',
+  'value': "Yüzbaşı Hüseyin Ağa'nın eytamına bazı levazımatın iştirası için 2 adet fransız lirası itasına karar verilmesi hususunda vasileri Fuznaraki Hayati Efendi'nin talepte bulunduğuna dair Hanya Emval-i Eytam Müdürünün yazısı. [19 Kanun-i sani 1308]",
+  'code': 28},
+ {'type': 'Document',
+  'value': "Müteveffa Yüzbaşı Hüseyin Ağa'nın eytamı Kazım, Şahsene ve Fatma'nın Teşrin-i Sani-Kanun-i Evvel 1306 dönemine ait 2 aylık nafakaları olan 400 kuruşu Hanya Eytam Sandığı'ndan aldığına dair (?) Hasan imzalı pusula. [3 Kanun-i Evvel 1306]",
+  'code': 29},
+ {'type': 'Document',
+  'value': "Hanya mahalatından Musa Paşa Mahalesinde sakine Fatma Hanım binti Yüzbaşı Hüseyin Ağa'nın hastahane-i askeri eczacılarından bekraki Hüseyin Hakkı Efendi'yi vekil tayin ettiğine dair ilam.",
+  'code': 30},
+ {'type': 'Document',
+  'value': "Yüzbaşı Hüseyin Ağa'nın yetimleri Kazım Efendi ile Esna Hanım'ın bazı ihtiyaçlarına sarf olunmak üzere 418 kuruşun Hanya Eytam Sandığı’ndan alındığına dair pusula. [7 Teşrin-i Evvel 1309]",
+  'code': 31},
+ {'type': 'Document',
+  'value': 'Musa Paşa mahallesindeki bir hanenin tuvaletiyle bulaşıkhanesinin tamir ve tathirinden Yüzbaşı Hüseyin Ağa eytamının hanesinin 12 sehmden mutasarrıf oldukları 7 sehme ait 36 kuruş 7 paranın Hanya Eytam Sandığı’ndan alındığına dair pusula. [4 Mayıs 1307]',
+  'code': 32},
+ {'type': 'Document',
+  'value': "Yüzbaşı Hüseyin Ağa'nın yetimleri Kazım Efendi ile EsnaHanım'ın nafakaları olan 180 kuruşun Hanya Eytam Sandığı’ndan alındığına dair Mehmed imzalı pusula. [18 Teşrin-i Evvel 1309]",
+  'code': 33},
+ {'type': 'Document',
+  'value': "Yüzbaşı Hüseyin Ağa'nın eytamının Eylül 1308 nafakası olan 200 kuruşun vasileri tarafından Hanya Eytam Sandığı'ndan alındığına dair pusula. [14 Eylül 1308]",
+  'code': 34},
+ {'type': 'Document',
+  'value': "Yüzbaşı Hüseyin Ağa'nın yetimesi Esna'nın elbisesine verilmek üzere 150 kuruş itasına karar verilmesinin talep edildiğine dair yazı. [22 Teşrin-i Sani 1310]",
+  'code': 35},
+ {'type': 'Document',
+  'value': "Estiye Kazası Merkez Kaymakamlığı olan Avniye Kasabası ahalisinin yardımıyla, inşa ettiği camide karşılıksız olarak müezzinlik yapan Hüseyin Hakkı Efendi'nin kendisine maaş verilmesine dair talebi. [25 Temmuz 1307]",
+  'code': 36},
+ {'type': 'Document',
+  'value': "Esna Hanım'ın Hanya islam Eytam Sandığı'na medyun olduğu parayı faiziyle sandığa tesviye ve ita ettiği, bu suretle sandığın alacağı kalmadığından merhun olan 2 bab hanenin rehninin fek ve ihracı için medyuna ruhsat verildiğine dair ibraname. [5 Mayıs 1313]",
+  'code': 37},
+ {'type': 'Document',
+  'value': "Alaybeyi müteveffa Hasan Bey'in eşi Esna Hanım tarafından müteveffa eşinin biraderzadelerini Eytam Nezaretince kaydedilen yetimlerinin aleyhine daha önce açılmış olan 50 adet Fransız altını alacak davasınına dair karar.",
+  'code': 38},
+ {'type': 'Document',
+  'value': "Alaybeyi müteveffa Hasan beyin Musa Paşa Mahallesi'nde mutasarrıf olduğu konağın tamiri için keşif yapılması ve tamirine izin verilmesine dair Hanya Eytam Meclis-i Aliye'sine gönderilmiştir. arzı. [30 Eylül 1303]",
+  'code': 39},
+ {'type': 'Document',
+  'value': 'Eşi Aliye Hanıma hiddetlenip işine gitmek için evden çıktıktan sonra döndüğünde kapıyı açmadığını, taşraya gitmesi için izin vermediği halde gittiğini eşinin celbiyle nizamı dairesinde edilmesine dair Hamid Dra Arifakinin dilekçesi [29 Teşrin-i sani 1306]',
+  'code': 40},
+ {'type': 'Document',
+  'value': 'Gemici Hamid Dıraro Arifaki evine sarhoş gelip eşi Osmanapola Aliye Hanımı tard edip nahoş sözler söylemiş, elindeki bıçakla katl ve idam etmeğe tasaddi etmesi nedeniyle eşinin çağırılarak gereğinin yapılmasına dair taleb. [5 Kanun-i evvel 1306]',
+  'code': 41},
+ {'type': 'Document',
+  'value': 'Eşi Pirinaki Eminin şiddetine maruz kalan ve daha önce meclis tarafından barıştırılan ancak asla iyi bir netice vermediğinden Nuriye binti İbrahim Lukakinin eşi hakkında şikayet dilekçesi. [22 Ağustos 1306] [Hanya]',
+  'code': 42},
+ {'type': 'Document',
+  'value': "Hünkar Mahallesi'nde müştereken sahip oldukları evlerini bir başkasına satan ve kendisine kötü davranan eşi Pirinaki Emin'den şikayet eden Nuriye binti İbrahim Lukaki'nin dilekçesi. [29 Temmuz 1306]",
+  'code': 43},
+ {'type': 'Document',
+  'value': "Hanya ahalisinden Emin(?)'nın kendisini sebepsiz yere boşadığı mühr-i muacceli bulunan 11.500 akçe tesviye ita etmiş ise de nafakası için bir şey vermediğinden mahkemeye celbiyle icabının icrası hususunda Nuriye binti İbrahim’in dilekçesi. [2 Haziran 1308]",
+  'code': 44},
+ {'type': 'Document',
+  'value': "Salih Camadaki'nin Laşid İslam Eytam Sandığı'ndan aldığı 2200 kuruşun karşılığında terhinat kontratosu. [12 Eylül 1298]",
+  'code': 45},
+ {'type': 'Document',
+  'value': "İhtiyar Meclisi'nin 23 Şaban 1290 tarihli emirnamesine cevaben müteveffa Mehmed Reis'in borcunun tahsili emredilmiş, Salih Camadaki celp olunarak borcu istenmişse de fakirliği dolayısıyla borcunu tediyeye muktedir olamayacağına dair takrir.",
+  'code': 46},
+ {'type': 'Document',
+  'value': "Müteveffa Mehmed Reis'in Salih Çamadaki ile Berber Hacı Ali'den matlubu olup tahsili emrü irade buyurulan paradan bir kısmı Salih Çamadaki'nin deyni olup tesviye etmek için izam kılındığı gibi Hacı Ali'nin vereceği olan paranın irsal kılındığına dair takrir",
+  'code': 47},
+ {'type': 'Document',
+  'value': "Müteveffa Mehmed Reis'in eşi Penbe Hanım'a Mustafa Bey tarafından verilen 5 sim mecidiyeyi aldığına dair ilm-i haber.[12 Mayıs 1289]",
+  'code': 48}],
+  "links": 
+  [{'source': 0, 'target': 25,'weight':2},
+ {'source': 10, 'target': 25,'weight':1},
+ {'source': 19, 'target': 25,'weight':1},
+ {'source': 0, 'target': 26,'weight':2},
+ {'source': 11, 'target': 26,'weight':2},
+ {'source': 1, 'target': 27,'weight':9},
+ {'source': 12, 'target': 27,'weight':1},
+ {'source': 1, 'target': 28,'weight':9},
+ {'source': 13, 'target': 28,'weight':1},
+ {'source': 1, 'target': 29,'weight':9},
+ {'source': 14, 'target': 29,'weight':2},
+ {'source': 20, 'target': 29,'weight':1},
+ {'source': 15, 'target': 29,'weight':3},
+ {'source': 1, 'target': 30,'weight':9},
+ {'source': 15, 'target': 30,'weight':3},
+ {'source': 21, 'target': 30,'weight':1},
+ {'source': 1, 'target': 31,'weight':9},
+ {'source': 16, 'target': 31,'weight':1},
+ {'source': 22, 'target': 31,'weight':1},
+ {'source': 1, 'target': 32,'weight':9},
+ {'source': 3, 'target': 32,'weight':4},
+ {'source': 1, 'target': 33,'weight':9},
+ {'source': 15, 'target': 33,'weight':3},
+ {'source': 14, 'target': 33,'weight':2},
+ {'source': 1, 'target': 34,'weight':9},
+ {'source': 1, 'target': 35,'weight':9},
+ {'source': 3, 'target': 35,'weight':4},
+ {'source': 2, 'target': 36,'weight':1},
+ {'source': 3, 'target': 37,'weight':4},
+ {'source': 3, 'target': 38,'weight':4},
+ {'source': 4, 'target': 38,'weight':2},
+ {'source': 4, 'target': 39,'weight':2},
+ {'source': 5, 'target': 40,'weight':2},
+ {'source': 17, 'target': 40,'weight':1},
+ {'source': 6, 'target': 41,'weight':1},
+ {'source': 5, 'target': 41,'weight':2},
+ {'source': 7, 'target': 42,'weight':3},
+ {'source': 18, 'target': 42,'weight':3},
+ {'source': 7, 'target': 43,'weight':3},
+ {'source': 18, 'target': 43,'weight':3},
+ {'source': 7, 'target': 44,'weight':3},
+ {'source': 18, 'target': 44,'weight':3},
+ {'source': 8, 'target': 45,'weight':3},
+ {'source': 8, 'target': 46,'weight':3},
+ {'source': 9, 'target': 46,'weight':3},
+ {'source': 8, 'target': 47,'weight':3},
+ {'source': 9, 'target': 47,'weight':3},
+ {'source': 23, 'target': 47,'weight':1},
+ {'source': 9, 'target': 48,'weight':3},
+ {'source': 11, 'target': 48,'weight':2},
+ {'source': 24, 'target': 48,'weight':1}]
+}
+
+const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+// const color = d3.scaleSequential(d3.interpolateRainbow);
+const color = d3.scaleSequential(d3.interpolateRainbow)
+    .domain([9,12]);
+// const color = d3.scaleOrdinal(d3.schemeCategory20);
+const colorsArray = Array.from(Array(2).keys());
+
+const types = [
+"Individual",
+"Document"
+];
+
+// define constants
+const { nodes, links } = data;
+const headerHeight = document.getElementById('header').clientHeight;
+let w = window.innerWidth;
+let h = window.innerHeight * 1.2;
+const r = 18;
+const margin = {
+  left: 20,
+  right: 20,
+  top: 20,
+  bottom: 20,
+};
+
+
+				   
+
+const dragstarted = (d) => {
+  if (!d3.event.active) simulation.alphaTarget(0.05).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+const dragged = (d) => {
+  d.fx = clamp(d3.event.x, 0, w);
+  d.fy = clamp(d3.event.y, 0, h);
+}
+
+const dragended = (d) => {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
+
+//initialize svg
+const svg = d3.select('body')
+  .append("svg")
+  .attr('width', w - margin.right - margin.left)
+  .attr('height', h - margin.top - margin.bottom)
+  .attr("class", "graph")
+  .attr("id", "graph")
+  .style('top', `${headerHeight + 1}px`)
+  .style('left', `${(window.innerWidth / 2) - (w / 2)}px`);
+
+// initialize tooltips
+const tip = d3.select("body").append("div")
+  .attr("class", "d3-tip")
+  .style("display", "none")
+
+// initialize simulation
+const simulation = d3.forceSimulation()
+  // keep entire simulation balanced around screen center
+  .force('center', d3.forceCenter(w/2, h/2))
+  // pull toward center
+  .force('attract', d3.forceAttract()
+    .target([w/2, h/2])
+    .strength(0.01))
+  // cluster by region
+  .force('cluster', d3.forceCluster()
+    .centers(d => types.indexOf(d.type))
+    .strength(1)
+    .centerInertia(0.1))
+
+  .force("link", d3.forceLink())
+  .force('charge', d3.forceManyBody().strength(-10))
+  // apply collision with padding
+  .force("collide", d3.forceCollide().radius(20).strength(0));
+
+// initialize links
+const link = svg.append("g")
+  .attr("class", "link")
+  .selectAll("line")
+  .data(links)
+  .enter()
+  .append("line")
+  .attr("stroke-width", function(d){ return d.weight; });
+
+// initialize node circles
+const node = svg
+  .append("g")
+  .attr("class", "node")
+  .selectAll("circle")
+  .data(nodes)
+  .enter().append("circle")
+    .attr("r", r)
+    .attr("fill", d => color(types.indexOf(d.type)))
+    .attr("stroke", d => color(types.indexOf(d.type)))
+    .on("mouseover", tip.show)
+    .on("mouseout", tip.hide)
+    .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+// render flags
+d3.select('body').append('div').attr('class', 'flag__wrap');
+
+const flag = d3.select('.flag__wrap')
+  .attr('width', w - margin.right - margin.left)
+  .attr('height', h - margin.top - margin.bottom)
+  .style('top', `${headerHeight + 1}px`)
+  .style('left', `${(window.innerWidth / 2) - (w / 2)}px`)
+  .selectAll('.flag')
+  .data(nodes)
+  .enter()
+  .append()
+  .attr('id', d => d.code)
+  .attr('class', d => `flag flag-${d.code}`)
+  .on("mouseover", (d) => {
+     tip.style("display", "block")
+       .style("left", (d3.event.pageX + 8) + "px")
+       .style("top", (d3.event.pageY - 40) + "px");
+     tip.html(`<div class='tip-name'>${d.value}</div>`)
+       .attr('class', 'd3-tip');
+     })
+   .on("mouseout", (d) => {
+     tip.style("display", "none");
+     })
+  .call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended)
+    );
+
+ // define tick function
+const ticked = () => {
+  link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+
+  node
+    .attr("cx", d => `${d.x + 3}px`)
+    .attr("cy", d => `${d.y - 1}px`);
+
+  flag
+    .style('left', d => `${d.x + 12}px`)
+    .style('top', d => `${d.y + 12}px`);
+
+}
+
+// ramp up collision strength to provide smooth transition
+const transitionTime = 3000;
+const t = d3.timer((elapsed) => {
+  const dt = elapsed / transitionTime;
+  simulation.force('collide').strength(Math.pow(dt, 2) * 0.7);
+  if (dt >= 1.0) t.stop();
+});
+
+simulation
+    .nodes(nodes)
+    .on("tick", ticked);
+
+simulation.force("link")
+    .links(links);
+
+
+// initialize legend
+const legendRectSize = 18;
+const legendSpacing = 8;
+
+const legend = svg.selectAll('.legend')
+  .data(colorsArray)
+  .enter()
+  .append('g')
+  .attr('class', 'legend')
+  .attr('id', d => types[d])
+  .attr('transform', (d, i) => `translate(0, ${30 + (i * (legendRectSize + legendSpacing))})`);
+
+legend.append('circle')
+  .attr('r', legendRectSize / 2)
+  .attr('fill', d => color(d))
+  .attr('stroke', d => color(d))
+  .attr('cx', legendRectSize / 2)
+  .attr('cy', legendRectSize / 2);
+
+legend.append('text')
+  .attr('x', legendRectSize + legendSpacing)
+  .attr('y', legendRectSize - (legendSpacing * .5))
+  .text(d => types[d]);
+
